3adea6d628a8f237d271d2866909db8b06898525
cillian <cillianwxp@gmail.com>
2026-04-23 17:35:49 +0800

    Add Diffview Neovim plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -33,8 +33,12 @@ return {
     "nvim-mini/mini.animate",
     opts = function(_, opts)
       local animate = require("mini.animate")
+      opts.cursor = {
+        enable = false,
+      }
       opts.scroll = {
-        timing = animate.gen_timing.linear({ duration = 90, unit = "total" }),
+        enable = true,
+        timing = animate.gen_timing.linear({ duration = 20, unit = "total" }),
         subscroll = animate.gen_subscroll.equal(),
       }
     end,
@@ -50,6 +54,39 @@ return {
         "shfmt",
         "stylua",
         "typescript-language-server",
+      },
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewClose",
+      "DiffviewFileHistory",
+      "DiffviewFocusFiles",
+      "DiffviewToggleFiles",
+    },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+      { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
+      { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Repo History" },
+      { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+    },
+    opts = {
+      enhanced_diff_hl = true,
+      use_icons = true,
+      view = {
+        merge_tool = {
+          layout = "diff3_mixed",
+          disable_diagnostics = true,
+        },
+      },
+      file_panel = {
+        win_config = {
+          position = "left",
+          width = 35,
+        },
       },
     },
   },
